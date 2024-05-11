16d19bd16989ddc460582105759e9061bb5c71f2
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-05-11 14:49:37 -0300

    :sparkles: added TFTP recovery method

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,35 +1,20 @@
 #!/bin/ash
+# Author: Arthur Cadore M. Barcella
+# Github: arthurcadore
 
-# create a new user for 
-useradd x1
-mkdir -p /data/x1
-chown x1:ftp /data/x1
+# Script to initialize the TFTP server.
 
-sed -i '$d' /etc/passwd
-echo 1:x:1001:1001::/data/x1: >> /etc/passwd
+# Start the TFTP server and keep it running sharing the directory tftpshare.
+# The TFTP is listening on port 69 of any interface
 
-sed -i '$d' /etc/group
-echo 1:!:1001: >> /etc/group
+# Start the TFTP server
+in.tftpd -l -s /tftpshare -a 0.0.0.0:69 &
 
-sed -i '$d' /etc/shadow
-echo 1:!:19854:0:99999:7::: >> /etc/shadow
+# Verify the status of the TFTP server:
+ps aux
 
-echo -e "1\n1" | passwd 1
+# Verify the status of the TFTP port:
+netstat -lnu
 
-useradd x2
-mkdir -p /data/x2
-chown x2:ftp /data/x2
-
-sed -i '$d' /etc/passwd
-echo 2:x:1002:1002::/data/x2: >> /etc/passwd
-
-sed -i '$d' /etc/group
-echo 1:!:1002: >> /etc/group
-
-sed -i '$d' /etc/shadow
-echo 2:!:19854:0:99999:7::: >> /etc/shadow
-
-echo -e "2\n2" | passwd 2
-
-# starts FTP-server
-/start-ftp
+# Keep the container running
+tail -f /dev/null
